3a383c92d1579e0a497c1fe6c0815aae198dbb95
Parth Bhatia <parthbhatia@Parths-MacBook-Air.local>
2024-07-07 18:38:43 +0530

    first basic commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+# import yfinance as yf
+
+# st.title("Stocks App")
+# symbol = st.text_input("Enter a stock symbol", "AAPL")
+# if st.button("Get Quote"):
+#     st.json(yf.Ticker(symbol).info)
+
+
+st.title('_Exercise_ _Recommender_ :runner:')
+st.title('_Streamlit_ is :blue[cool] :sunglasses:')
+st.write('Hello world')
+st.markdown('**Hello world!**')
+st.write('Running Streamli')
